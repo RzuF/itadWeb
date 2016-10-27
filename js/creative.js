19139c84e41dfe62e5808acc544d47433c675290
Tomasz Błahut <rzuf22@gmail.com>
2016-10-27 12:05:02 +0200

    initial commit

---
+++ js/creative.js
@@ -0,0 +1,108 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+$(document).ready(function () {
+    //alert($(".table").width() + 36);
+    var $x = ($(".table").width() + 36);
+
+    //alert($(window).height() + " na " + $(window).width());
+
+    $(".pricing-table").css({
+        "max-width": $x
+    });    
+
+    var $bNavH = ($("#footer").height() + 10);
+    $(".bottom-padding").css({
+        "padding-bottom": $bNavH
+    })
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = 'November 16 2016 09:00:00 UTC+0100';
+    initializeClock('clockdiv', deadline);
+});
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();    
+
+    
+
+})(jQuery); // End of use strict
---
+++ js/fade.js
@@ -0,0 +1,26 @@
+﻿$(document).scroll(function () {
+
+    var pos = $(".header-content-inner").offset().top + $(".btn").height();
+
+    var y = $(this).scrollTop();
+    if (y > pos) {
+        //$('.bNav').removeClass('hidden');
+        $('#footer').fadeIn();
+
+        //alert("#1");
+    } else {
+        $('#footer').fadeOut();
+
+        //alert(y);
+    }
+
+    var $spotkaniaH = $("#spotkania").height() == 0 ? 153 : $("#spotkania").height();
+    $("#BIS").css({
+        "max-height": $spotkaniaH
+    })
+
+    $("#radio").css({
+        "max-height": $spotkaniaH
+    })
+
+});
